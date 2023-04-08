d0ffe701d8eac210361ee6c85afec897705ad187
Ruslan Rohovskyi <hazekk94@gmail.com>
2023-04-08 02:29:28 +0100

    Username screen, booking movie session, tickets list, localStorage saving data, desktop layout

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,370 @@
+
+//https://github.com/hazed0m/digital-suits-cinema-tickets.git
+
+// Реализовать одностраничное приложение резервирования билетов в кинотеатр.
+// Время начала сеансов 10.00, время последнего сеанса 20.00. Шаг каждые - 2 часа.
+// Таким образом в течение дня может быть 6 сеансов.
+// Интерфейс должен отображать доступные даты для бронирования,
+// сеансы для выбранной даты, свободные и забронированные места.
+// Максимальное число мест 4 - 5. Все данные о билетах можно хранить либо в JSON, либо сделать статикой.
+// Для хранения дат использовать LocalStorage, и при перезагрузке страницы считывать данные из LocalStorage.
+// Максимальный период бронирования: одна неделя от текущей даты.
+// Проверить совместимость с современными браузерами (Safari, Google Chrome, FireFox, Edge)
+// Реализовать на JavaScript/JQuery/HTML5/CSS3
+const selectors = {
+    'sessionWrapper': ".movies__session",
+    'seatsWrapper': '.session__seats-wrapper',
+    'seatItem': '.session__seat',
+    'timeWrapper': '.movies__time-wrapper',
+    'moviesWrapper': '.movies__wrapper',
+    'moviesItemWrapper': '.movies__item-wrapper',
+    'moviesItem': '.movies__item',
+    'localStorage': 'monthSession',
+    'register': '.register',
+    'registerWrapper': '.register__wrapper',
+    'registerForm': '.register__form',
+    'container': '.container',
+    'tickets': '.tickets',
+    'ticketsWrapper': '.tickets__wrapper',
+    'ticketsTitle': '.tickets__title'
+};
+const schemas = {
+    daySessionSchema: {
+        'A': {},
+        'B': {}
+    },
+    seatList: {
+        top: false, left: false, bottom: false, right: false
+    },
+    monthsList: [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+};
+const modifiers = {
+    active: 'active',
+    gone: 'gone',
+    done: 'done'
+}
+const data = {
+    sessionsChart: [
+        {
+            movieTitle: "John Wick 4",
+            movieCover: "johnwick4.jpeg",
+            movieGenre: "Action",
+            movieId: 120,
+            hallName: 'A',
+            playingHours: {
+                10: schemas.seatList, 12: schemas.seatList, 14: schemas.seatList, 16: schemas.seatList, 18: schemas.seatList, 20: schemas.seatList
+            }
+        },
+        {
+            movieTitle: "The Pope's Exorcist",
+            movieCover: "thepopesexsorcist.jpeg",
+            movieGenre: "Thriller",
+            movieId: 122,
+            hallName: 'B',
+            playingHours: {
+                18: schemas.seatList, 20: schemas.seatList
+            }
+        },
+        {
+            movieTitle: "Super Mario Movie",
+            movieCover: "mariobrosmovie.jpeg",
+            movieGenre: "Animation",
+            movieId: 135,
+            hallName: 'B',
+            playingHours: {
+                10: schemas.seatList, 12: schemas.seatList, 14: schemas.seatList, 16: schemas.seatList
+            }
+        }
+    ],
+    monthSession: null,
+    bookedSeat: null,
+    userName: 'Anonymous',
+    tickets: []
+}
+
+const globalDate = new Date();
+
+function generateMonthSession() {
+    const weekDayCount = 7; // only 7 days from current date
+    const currentDate = globalDate.getDate();
+    const currentMonth = globalDate.getMonth();
+    const currentMonthName = schemas.monthsList[currentMonth];
+    const lastDate = new Date(globalDate.getFullYear(), globalDate.getMonth() + 1, 0).getDate();
+    const availableSessionsList = {};
+    const daySessionSchema = generateDaySession();
+
+    if(currentDate + weekDayCount <= lastDate) {
+        availableSessionsList[currentMonthName] = {};
+
+        for (let i = 0; i <= weekDayCount; i++) {
+            availableSessionsList[currentMonthName][`${currentDate + i}`] = daySessionSchema;
+        }
+    }
+    return availableSessionsList;
+}
+function generateDaySession() {
+    data.sessionsChart.forEach(function(item, index) {
+        schemas.daySessionSchema[item.hallName][`${item.movieId}`] = item;
+    });
+    return schemas.daySessionSchema;
+}
+function generatePlayingHours(hoursList, movieId, hallName) {
+    const seatIcon = ``;
+    let seatTemplate = ``;
+    let hoursTemplate = ``;
+    let timeWrapper = ``;
+    let currentTime = 0;
+    let iterator = 0;
+    const currentHour = globalDate.getHours();
+
+    for (const [time, seats] of Object.entries(hoursList)) {
+        //check to remove finished or started sessions
+        if(time >= currentHour) {
+            //Render time-wrapper
+            timeWrapper += `
+                <div data-time="${time}" class="movies__time${iterator == 0 ? ' active' : ''}">${time}:00</div>
+            `;
+            //Start session__seats-wrapper
+            seatTemplate += `<div data-time="${time}" class="session__seats-wrapper${iterator == 0 ? ' active' : ''} border-shadow">`;
+            //Seats till next hour
+            for (const [title, seat] of Object.entries(seats)) {
+                seatTemplate += `
+                    <div seat-status="${seat}" hall-name="${hallName}" movie-id="${movieId}" seat-position="${title}" seat-position="${title}" class="session__seat${seat != false ? ` ${modifiers.gone}` : ''}">
+                        ${seatIcon}
+                    </div>
+                `;
+            }
+            //End session__seats-wrapper
+            seatTemplate += `<div class="session__seat-time">${time}:00</div>`;
+            seatTemplate += `</div>`;
+            hoursTemplate = `
+                <div class="movies__time-wrapper">
+                    ${timeWrapper}
+                </div>
+                <div class="movies__session">
+                    ${seatTemplate}
+                </div>
+            `;
+            iterator++;
+        }
+    }
+    return hoursTemplate;
+}
+function generateMovieItem(sessions) {
+    if(sessions != undefined) {
+        let movieTemplate = ``;
+        for (const hall in sessions) {
+            const hallMoviesList = sessions[hall];
+            const hallWrapper = `<div class="movies__hall">Hall "${hall}"</div>`;
+            movieTemplate += hallWrapper;
+            for (const movieItemKey in hallMoviesList) {
+                const movieItem = hallMoviesList[movieItemKey];
+                movieTemplate += `
+                <div class="movies__item border-shadow">
+                    <div class="movies__image-wrapper border-shadow">
+                        <picture>
+                            <img class="movies__image img" src="assets/images/${movieItem.movieCover}" alt="${movieItem.movieTitle}" loading="lazy" decoding="async">
+                        </picture>
+                    </div>
+                    <div class="movies__title-wrapper">
+                        <div class="movies__title">${movieItem.movieTitle}</div>
+                        <div class="movies__genre">${movieItem.movieGenre}</div>
+                    </div>
+                    ${generatePlayingHours(movieItem.playingHours, movieItem.movieId, hall)}
+                </div>
+            `;
+            };
+        }
+        return movieTemplate;
+    }
+}
+function setLocalStorage(localStorageData) {
+    if(localStorageData != undefined) {
+        localStorage.setItem(selectors.localStorage, JSON.stringify(localStorageData));
+    } else {
+        if(localStorage.getItem(selectors.localStorage) === null) {
+            localStorage.setItem(selectors.localStorage, JSON.stringify(data.monthSession));
+        } else {
+            const localStorageObject = JSON.parse(localStorage.getItem(selectors.localStorage));
+        }
+    }
+}
+function getLocalStorage() {
+    const localStorageObject = JSON.parse(localStorage.getItem(selectors.localStorage));
+    return localStorageObject;
+}
+function eventsLoader() {
+    const timeWrapper = $(selectors.timeWrapper);
+    const seatsWrapper = $(selectors.seatsWrapper);
+    const register = $(selectors.register);
+    const container = $(selectors.container);
+    const registerForm = $(selectors.registerForm);
+    const tickets = $(selectors.tickets);
+    const ticketsTitle = $(selectors.ticketsTitle);
+    const ticketsWrapper = $(selectors.ticketsWrapper);
+
+    timeWrapper.on('click', function(e) {
+        const clickTarget = $(e.target);
+
+        const currentTime = clickTarget.attr('data-time');
+        const moviesItemWrapper = clickTarget.closest(selectors.moviesItemWrapper);
+        const moviesItem = clickTarget.closest(selectors.moviesItem);
+        const date = moviesItemWrapper.attr('data-date');
+        const month = moviesItemWrapper.attr('data-month');
+        const activeSessionWrapper = moviesItem.find(`${selectors.timeWrapper} .${modifiers.active}`);
+        const activeSeatsWrapper = moviesItem.find(`${selectors.seatsWrapper}.${modifiers.active}`);
+
+        if(currentTime !== undefined) {
+            const currentSeatWrapper = moviesItem.find(`${selectors.seatsWrapper}[data-time="${currentTime}"]`);
+            activeSessionWrapper.removeClass(modifiers.active);
+            activeSeatsWrapper.removeClass(modifiers.active);
+            clickTarget.addClass(modifiers.active);
+            currentSeatWrapper.addClass(modifiers.active);
+        }
+    });
+    seatsWrapper.on('click', function(e) {
+        const clickTarget = $(e.target);
+        if(!clickTarget.hasClass(modifiers.gone)) {
+            const moviesItemWrapper = clickTarget.closest(selectors.moviesItemWrapper);
+            const seatsWrapper = clickTarget.closest(selectors.seatsWrapper);
+            const time = seatsWrapper.attr('data-time');
+            const date = moviesItemWrapper.attr('data-date');
+            const month = moviesItemWrapper.attr('data-month');
+            const currentSeat = clickTarget.attr('seat-position');
+            const hallName = clickTarget.attr('hall-name');
+            const movieId = clickTarget.attr('movie-id');
+            const localStorageData = getLocalStorage();
+            const movieTitle = localStorageData[month][date][hallName][movieId].movieTitle;
+
+            if(localStorageData !== undefined) {
+                localStorageData[month][date][hallName][movieId].playingHours[time][currentSeat] = true;
+                setLocalStorage(localStorageData);
+                clickTarget.addClass(modifiers.gone);
+
+                if(data.userName != '') {
+                    data.tickets.push({
+                        name: data.userName,
+                        time: time,
+                        date: date,
+                        month: month,
+                        movieId: movieId,
+                        movieTitle: movieTitle,
+                        currentSeat: currentSeat
+                    });
+                }
+                localStorage.setItem('tickets', JSON.stringify(data.tickets));
+                renderTickets();
+            }
+        }
+    });
+    registerForm.on('submit', function(e) {
+        e.preventDefault();
+        const register = $(selectors.register);
+        const container = $(selectors.container);
+        const registerInput = registerForm.find('#name');
+        const name = registerInput.val();
+        localStorage.setItem('name', name);
+        data.userName = name;
+        container.addClass(modifiers.active);
+        register.addClass(modifiers.done);
+        registerForm.find('#name').val();
+    });
+    ticketsTitle.on('click', function() {
+        if(!ticketsWrapper.hasClass(modifiers.active)) {
+            ticketsWrapper.addClass(modifiers.active);
+        } else {
+            ticketsWrapper.removeClass(modifiers.active);
+        }
+    });
+    ticketsWrapper.on('mouseleave', showTicketsWrapper);
+    container.on('mouseover', showTicketsWrapper);
+}
+function showTicketsWrapper() {
+    const ticketsWrapper = $(selectors.ticketsWrapper);
+    ticketsWrapper.removeClass(modifiers.active);
+}
+function getTickets() {
+    const ticketsLocalStorage = localStorage.getItem('tickets');
+    if(ticketsLocalStorage != undefined) {
+        const tickets = JSON.parse(ticketsLocalStorage);
+        data.tickets = tickets;
+        renderTickets();
+    }
+}
+function renderTickets() {
+    const ticketsWrapper = $(selectors.ticketsWrapper);
+    const tickets = $(selectors.tickets);
+
+    let ticketTemplate = ``;
+    tickets.addClass(modifiers.active);
+    ticketsWrapper.empty();
+    $(data.tickets).each(function(index, item) {
+        ticketTemplate += `
+            <div class="tickets__ticket-item">
+               <span class="tickets__ticket-info">Movie - ${item.movieTitle}</span>       
+               <span class="tickets__ticket-info">Seat - ${item.currentSeat}</span>  
+               <span class="tickets__ticket-info">Time - ${item.time}:00</span>
+               <span class="tickets__ticket-info">Date - ${item.date} ${item.month}</span>
+               <span class="tickets__ticket-info">Visitor - ${item.name}</span>                 
+            </div>
+        `;
+    });
+    ticketsWrapper.append(ticketTemplate);
+}
+function getName() {
+    const register = $(selectors.register);
+    const container = $(selectors.container);
+    const nameLocalStorage = localStorage.getItem('name');
+
+    if(nameLocalStorage != undefined) {
+        data.userName = nameLocalStorage;
+        register.addClass(modifiers.done);
+        container.addClass(modifiers.active);
+    }
+}
+function render() {
+    const moviesContainer = $(selectors.moviesWrapper);
+    const localStorageData = getLocalStorage();
+    let currentData = null;
+    let months = null;
+
+    if(localStorageData != undefined) {
+        currentData = localStorageData;
+        months = Object.keys(localStorageData)
+    } else {
+        data.monthSession = generateMonthSession();
+        currentData = data.monthSession;
+        months = Object.keys(data.monthSession);
+    }
+
+    if(months != null && currentData != null) {
+        //Clean container
+        moviesContainer.empty();
+
+        //Render each month
+        $(months).each(function (index, month) {
+            for (const innerKey in currentData[month]) {
+                const innerItem = currentData[month][innerKey];
+                const dayWrapper = `<div class="movies__day-wrapper">
+                                            <div class="movies__day-title border-bottom">${innerKey} ${month}</div>
+                                        </div>`;
+                const generateMovieTemplate = generateMovieItem(innerItem);
+                if (generateMovieTemplate != '') {
+                    const template = `<div class="movies__item-wrapper" data-date="${innerKey}" data-month="${month}">
+                                        ${dayWrapper}
+                                        ${generateMovieTemplate}
+                                    </div>`;
+                    moviesContainer.append(template);
+                }
+            }
+        });
+    }
+    getName();
+    getTickets();
+    setLocalStorage();
+    eventsLoader();
+}
+
+render();
